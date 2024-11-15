fd0271a3dab1e7336d1d48833f345b804ff09623
aselturatbekk <aselturatbekk@gmail.com>
2024-11-15 22:17:25 +0300

    loop like icon updated

--- navision/src/assets/icons/loopicons/HeartLoop.js
+++ navision/src/assets/icons/loopicons/HeartLoop.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Svg, { Path } from 'react-native-svg';
 
-const HeartIcon = (props) => (
+const HeartIcon = ({ color = 'white', ...props }) => (
   <Svg
     width={20}
     height={17}
@@ -12,7 +12,7 @@ const HeartIcon = (props) => (
   >
     <Path
       d="M10.397 16.3233C10.0836 16.4339 9.56744 16.4339 9.25406 16.3233C6.58113 15.4108 0.608521 11.6042 0.608521 5.1523C0.608521 2.30425 2.90355 0 5.73317 0C7.41066 0 8.8946 0.811096 9.82552 2.06461C10.7564 0.811096 12.2496 0 13.9179 0C16.7475 0 19.0425 2.30425 19.0425 5.1523C19.0425 11.6042 13.0699 15.4108 10.397 16.3233Z"
-      fill="white"
+      fill={color}
     />
   </Svg>
 );
