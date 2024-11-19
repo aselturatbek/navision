8c4defc620ac644e35a1ccfe9386aa74b657f24f
aselturatbekk <aselturatbekk@gmail.com>
2024-11-19 17:06:57 +0300

    update profile screen

--- navision/src/screens/ProfileScreen.js
+++ navision/src/screens/ProfileScreen.js
@@ -190,7 +190,7 @@ const ProfileScreen = () => {
   
 
   if (!userInfo) {
-    return <Text>Loading...</Text>; // Veriler yüklenirken gösterilecek
+    return <Text>Loading</Text>; // Veriler yüklenirken gösterilecek
   }
 
   return (
