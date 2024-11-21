a6789554152263264856edfd788359fff4823299
aselturatbekk <aselturatbekk@gmail.com>
2024-11-21 21:08:46 +0300

    api_base_url in .env

--- navision/babel.config.js
+++ navision/babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true); // Performans için cache mekanizması
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo'], // Expo için gerekli Babel preset'i
+    plugins: [
+      [
+        'module:react-native-dotenv', // Dotenv dosyası için eklenti
+        {
+          moduleName: '@env', // Değişkenleri @env'den çağırmak için ayar
+          path: '.env', // .env dosyasının yolu
+        },
+      ],
+    ],
   };
 };
